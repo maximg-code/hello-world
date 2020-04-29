b5c3ef2b255ed34f62fc953a80cea143686c08ee
MaximG <maxgzv@gmail.com>
2020-04-29 09:32:23 +0400

    First app "Hello world!"

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+        i = i+1;
+        System.out.println(i);
+        i = i+1;
+        System.out.println(i);
+        i = i+1;
+        System.out.println("Hello world!" + i);
+
+        String hello = "Hello ";
+        String world = "world!";
+        System.out.println(hello + world);
+    }
+}
